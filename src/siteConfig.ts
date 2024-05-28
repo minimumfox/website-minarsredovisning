d0463f19ae2bf6c0be124ec22bd38f8a2335eb07
Martin Olsson <martin@minimum.se>
2024-05-28 09:42:50 +0200

    Better og:description

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -1,10 +1,10 @@
 export const SITE_TITLE = 'Min Årsredovisning'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const PRICE_EXCL_VAT = '640'
+export const SITE_DESCRIPTION = `Skapa årsredovisning/deklaration för mindre aktiebolag (K2), ${PRICE_EXCL_VAT} kr ex moms`
 export const SITE_EMAIL = 'info@minarsredovisning.se'
 export const LOGIN_URL = 'https://app.minarsredovisning.se/start-email-login'
 export const SOCIAL_FACEBOOK_URL = 'https://www.facebook.com/people/Min-%C3%85rsredovisning/100091410733509/'
 export const SOCIAL_LINKEDIN_URL = 'https://www.linkedin.com/company/min-arsredovisning/'
-export const PRICE_EXCL_VAT = '640'
 
 export const getAppUrl = () => {
     if (import.meta.env.TARGET_ENV === 'production') {
