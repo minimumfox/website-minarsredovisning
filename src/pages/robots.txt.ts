9c5a2e7094f37089a7ea1beb8424aad16aed79d0
Martin Olsson <martin@minimum.se>
2024-06-14 18:37:31 +0200

    Add sitemap to robots.txt and as <link> tag

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,10 +2,17 @@ import type { APIContext } from 'astro'
 import dedent from 'dedent'
 
 export async function GET(_context: APIContext) {
-    const BLOCK_ROBOTS =
-        dedent`
-    User-agent: *
-    Disallow: /
-  ` + '\n'
-    return new Response(import.meta.env.TARGET_ENV === 'staging' ? BLOCK_ROBOTS : '')
+    let robotsTxt = ''
+    if (import.meta.env.TARGET_ENV !== 'production') {
+        robotsTxt += dedent`
+            User-agent: *
+            Disallow: /
+            \n`
+    }
+    robotsTxt += `Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}\n`
+    return new Response(robotsTxt, {
+        headers: {
+            'Content-Type': 'text/plain; charset=utf-8',
+        },
+    })
 }
