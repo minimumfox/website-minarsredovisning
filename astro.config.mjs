69b7c279f794412b34f64bacd0fb6fed84865332
Martin Olsson <martin@minimum.se>
2024-06-14 18:43:45 +0200

    Set dynamic website URL in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-
 import sitemap from '@astrojs/sitemap'
+
+import { getWebsiteUrl } from './src/siteConfig.ts'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.minarsredovisning.se',
+    site: getWebsiteUrl(process.env.TARGET_ENV),
     integrations: [mdx(), sitemap()],
 })
--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -1,23 +1,36 @@
+export const getWebsiteUrl = (targetEnv: string) => {
+    if (targetEnv === 'production') {
+        return 'https://www.minarsredovisning.se'
+    } else if (targetEnv === 'development') {
+        return 'http://localhost:4321'
+    } else if (targetEnv === 'staging') {
+        return 'https://webstaging.minarsredovisning.se'
+    } else {
+        throw Error(`unknown TARGET_ENV "${targetEnv}"`)
+    }
+}
+
+/* Note: this is the URL to the app, config for the URL to the marketing website is in astro.config.mjs */
+export const getAppUrl = () => {
+    const targetEnv = process.env.TARGET_ENV
+    if (targetEnv === 'production') {
+        return 'https://app.minarsredovisning.se'
+    } else if (targetEnv === 'development') {
+        return 'http://localhost:4000'
+    } else if (targetEnv === 'staging') {
+        return 'https://staging.minarsredovisning.se'
+    } else {
+        throw Error(`unknown TARGET_ENV "${targetEnv}"`)
+    }
+}
+
 export const SITE_TITLE = 'Min Årsredovisning'
 export const PRICE_EXCL_VAT = 640
 export const SITE_DESCRIPTION = `Skapa årsredovisning/deklaration för mindre aktiebolag (K2), ${PRICE_EXCL_VAT} kr ex moms`
 export const SITE_EMAIL = 'info@minarsredovisning.se'
-export const LOGIN_URL = 'https://app.minarsredovisning.se/start-email-login'
+export const LOGIN_URL = `${getAppUrl()}/start-email-login`
 export const SOCIAL_FACEBOOK_URL = 'https://www.facebook.com/people/Min-%C3%85rsredovisning/100091410733509/'
 export const SOCIAL_LINKEDIN_URL = 'https://www.linkedin.com/company/min-arsredovisning/'
-
-/* Note: this is the URL to the app, config for the URL to the marketing website is in astro.config.mjs */
-export const getAppUrl = () => {
-    if (import.meta.env.TARGET_ENV === 'production') {
-        return 'https://app.minarsredovisning.se'
-    } else if (import.meta.env.TARGET_ENV === 'development') {
-        return 'http://localhost:4000'
-    } else if (import.meta.env.TARGET_ENV === 'staging') {
-        return 'https://staging.minarsredovisning.se'
-    } else {
-        throw Error(`unknown TARGET_ENV "${import.meta.env.TARGET_ENV}"`)
-    }
-}
 
 export const getPriceExclVat = () => PRICE_EXCL_VAT
 export const getPriceVat = () => Number((PRICE_EXCL_VAT * 0.25).toFixed(2))
